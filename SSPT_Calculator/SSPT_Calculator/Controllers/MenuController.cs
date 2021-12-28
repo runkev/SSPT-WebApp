79fdba0b68c61d3dbe9ef4ecf22f18be4bcadeef
Kevin Petow <kevin.m.petow@outlook.com>
2021-12-28 16:07:57 -0500

    created views and models for initial test info and vcg

---
+++ SSPT_Calculator/SSPT_Calculator/Controllers/MenuController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SSPT_Calculator.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult EnterTestInfo()
+        {
+            return View();
+        }
+        public IActionResult VCGR()
+        {
+            return View();
+        }
+        public IActionResult TestWeightInfo()
+        {
+            return View();
+        }
+        public IActionResult WeightByDeck()
+        {
+            return View();
+        }
+        public IActionResult VesselDimensions()
+        {
+            return View();
+        }
+        public IActionResult ImmersionMark()
+        {
+            return View();
+        }
+        public IActionResult WindHeel()
+        {
+            return View();
+        }
+        public IActionResult HMR()
+        {
+            return View();
+        }
+        public IActionResult WeightMovement()
+        {
+            return View();
+        }
+        public IActionResult FinalImmersion()
+        {
+            return View();
+        }
+        public IActionResult TestSummary()
+        {
+            return View();
+        }
+    }
+}
---
+++ SSPT_Calculator/SSPT_Calculator/Models/TestInfo.cs
@@ -0,0 +1,39 @@
+﻿namespace SSPT_Calculator.Models
+{
+    public class TestInfo
+    {
+        //Initial Test Info
+        public int weight { get; set; } //usually 185 lbs
+        public int pax { get; set; }
+        public int paxWeight { get; set; } //total weight of passengers
+        public int crew { get; set; }
+        public int crewWeight { get; set; }
+        public string routeCOI { get; set; } //oceans, coastwise, limited CW, lakes/bays/sounds, rivers
+        public string routeStability { get; set; } //exposed, partially protected, protected
+
+        //VCGR
+        public int paxStand { get; set; }
+        public int paxSit { get; set; }
+        public int vcgr { get; set; }
+
+        //Weight by deck variables
+        public int decks { get; set; }
+        public int paxUpper { get; set; }
+        public int upCheck { get; set; }
+        public int weightUpper { get; set; }
+        public int weightMain { get; set; }
+
+        //Vessel Dimension Values
+        public int loa { get; set; }
+        public int lr { get; set; }
+        public int fb { get; set; }
+        public int fs { get; set; }
+        public int bf { get; set; }
+        public int b { get; set; }
+        public int bp { get; set; }
+        public int f { get; set; }
+        public int fg { get; set; }
+        public int d { get; set; }
+
+    }
+}
